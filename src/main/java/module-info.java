65a3300b249c2db5dd8089ba63be8b38497735fc
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-04-26 23:37:40 -0700

    Had to add one more thing... jdk.unsupported to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.mangorage.installer {
     requires static com.google.gson;
     requires static jopt.simple;
+    requires jdk.unsupported;
 
     uses org.mangorage.installer.Installer;
 }
