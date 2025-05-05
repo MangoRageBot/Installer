cf0c7e65d2a978cd7edf56198891a4bfd35b8e01
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-05-04 19:44:53 -0700

    Fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.mangorage.installer {
     requires static com.google.gson;
     requires static jopt.simple;
     requires jdk.unsupported;
-    requires java.xml;
 
     uses org.mangorage.installer.Installer;
 }
